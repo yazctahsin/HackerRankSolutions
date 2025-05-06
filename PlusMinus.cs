5e682cadc7e8511cf680293dbe2f665fe5c4940e
Tahsin Yazıcı <117100542+yazctahsin@users.noreply.github.com>
2025-05-06 21:41:05 +0300

    Create PlusMinus.cs
    Plus Minus Solutions

---
+++ PlusMinus.cs
@@ -0,0 +1,16 @@
+ int total = arr.Count;
+    int posCount = 0, negCount = 0, zeroCount = 0;
+
+    foreach (int num in arr)
+    {
+        if (num > 0)
+            posCount++;
+        else if (num < 0)
+            negCount++;
+        else
+            zeroCount++;
+    }
+
+    Console.WriteLine("{0:F6}", (double)posCount / total);
+    Console.WriteLine("{0:F6}", (double)negCount / total);
+    Console.WriteLine("{0:F6}", (double)zeroCount / total);
