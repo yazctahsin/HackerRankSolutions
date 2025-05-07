8615ffc1474989742568a54c4de529f6e0cc59b2
Tahsin Yazıcı <117100542+yazctahsin@users.noreply.github.com>
2025-05-07 23:35:11 +0300

    Update PlusMinus.cs
    Edited.

--- PlusMinus.cs
+++ PlusMinus.cs
@@ -1,4 +1,6 @@
- int total = arr.Count;
+public static void plusMinus(List<int> arr)
+    {
+        int total = arr.Count;
     int posCount = 0, negCount = 0, zeroCount = 0;
 
     foreach (int num in arr)
@@ -14,3 +16,5 @@
     Console.WriteLine("{0:F6}", (double)posCount / total);
     Console.WriteLine("{0:F6}", (double)negCount / total);
     Console.WriteLine("{0:F6}", (double)zeroCount / total);
+
+    }
