57c20a96fdda42bdb54574cbed890ad61edf797e
Tahsin Yazıcı <117100542+yazctahsin@users.noreply.github.com>
2025-05-07 17:18:13 +0300

    Create simpleArraySum.cs
    Simple Array Sum Solution

---
+++ simpleArraySum.cs
@@ -0,0 +1,10 @@
+public static int simpleArraySum(List<int> ar)
+    {
+        int toplam = 0;
+        foreach(int a in ar)
+        {
+            toplam += a;            
+        }
+        return toplam;
+
+    }
